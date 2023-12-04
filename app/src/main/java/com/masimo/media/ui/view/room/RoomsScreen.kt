671ff4a27becb0f51a4e78e9813a95396b65f385
Tony Montgomery <tony.montgomery@soundunited.com>
2023-12-04 09:18:06 -0500

    DMS-124762: Self Development: Complete HEOS App coding challenge
    - don't COPY the room list, edit the real rooms list

--- app/src/main/java/com/masimo/media/ui/view/room/RoomsScreen.kt
+++ app/src/main/java/com/masimo/media/ui/view/room/RoomsScreen.kt
@@ -19,54 +19,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.masimo.media.data.MainViewModel
-import com.masimo.media.data.Room
 
 
 @Composable
 fun RoomsScreen(viewModel: MainViewModel) {
     var selectedIndex by remember { mutableStateOf(-1) }
 
-    val items by remember {
-        mutableStateOf((viewModel.roomList).map {
-            Room(
-                deviceName = it.deviceName,
-                deviceId = it.deviceId,
-                trackName = it.trackName,
-                albumName = it.albumName,
-                duration = it.duration,
-                artistName = it.artistName,
-                isPlaying = it.isPlaying, //playing/paused
-                image = it.image,
-                artSmall = it.artSmall,
-                artLarge = it.artLarge,
-                userSelected = it.userSelected
-            )
-        })
-    }
-
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .selectableGroup()
     ) {
-        items(items.size) { index ->
+        items(viewModel.roomList.size) { index ->
             Card(modifier = Modifier
                 .fillMaxWidth()
                 .selectable(selected = selectedIndex == index, onClick = {
                     selectedIndex = index
-                    viewModel.selectedRoom = items[index]
-                    viewModel.selectedRoomID = items[index].deviceId
-                    items[index].userSelected = true
+                    viewModel.selectedRoom = viewModel.roomList[index]
+                    viewModel.selectedRoomID = viewModel.roomList[index].deviceId
+                    viewModel.roomList[index].userSelected = true
                 })
 
                 .padding(16.dp),
                 elevation = 2.dp,
-                backgroundColor = if (viewModel.selectedRoomID == items[index].deviceId) Color.Green else Color.DarkGray,
+                backgroundColor = if (viewModel.selectedRoomID == viewModel.roomList[index].deviceId) Color.Green else Color.DarkGray,
                 shape = RoundedCornerShape(corner = CornerSize(16.dp))) {
 
                 Row {
-                    val isSelected = viewModel.selectedRoomID == items[index].deviceId
-                    RoomCard(room = items[index], isSelected)
+                    val isSelected = viewModel.selectedRoomID == viewModel.roomList[index].deviceId
+                    RoomCard(room = viewModel.roomList[index], isSelected)
                 }
             }
         }
